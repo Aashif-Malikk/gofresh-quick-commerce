f29347e9dd5b296daeeefa739eefde6d355ff56f
Aashif Malik <aashifmalik776@gmail.com>
2026-06-28 18:04:25 +0530

    update API_BASE

--- Client/src/Component/Vendor/VendorNav.jsx
+++ Client/src/Component/Vendor/VendorNav.jsx
@@ -1,7 +1,8 @@
 import { ArrowDown, ArrowDown2, DiscountShape, HambergerMenu, Location, User } from 'iconsax-react';
 import React, { useEffect, useRef, useState } from 'react'
 import { useNavigate, Link } from 'react-router-dom';
-const API_BASE = 'http://localhost:3000'
+const API_BASE = import.meta.env.VITE_API_URL || 'http://localhost:3000'
+
 
 
 function VendorNav() {
